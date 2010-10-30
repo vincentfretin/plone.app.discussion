07b38d7d60fe18ca2009c3e3b3db55dd3a30fc64
Silvio Tomatis <silvio@gropen.net>
2010-10-30 14:06:29 +0000

    Pseudo-test case to help development of ajax loaded comments
    svn path=/plone.app.discussion/branches/otacon-ajax/; revision=40931

---
+++ plone/app/discussion/tests/test_ajax_load.py
@@ -0,0 +1,63 @@
+'''
+This is not a "real" test file.
+I'm using it to develop ajax loading of comments.
+I'll converti it to a Selenium test soon.
+This should be a starting point: https://weblion.psu.edu/svn/weblion/weblion/assessmentmanagement.core/trunk/assessmentmanagement/core/selenium/testSelenium.py
+
+This too: http://pastebin.com/Dnx4WSMk
+'''
+from Products.PloneTestCase.ptc import PloneTestCase
+from plone.app.discussion.tests.layer import DiscussionLayer
+from Products.CMFCore.utils import getToolByName
+
+
+from plone.app.discussion.interfaces import IConversation
+
+from zope.component import createObject
+import unittest
+
+
+class AjaxLoadTest(PloneTestCase):
+
+    layer = DiscussionLayer
+
+    def afterSetUp(self):
+        # First we need to create some content.
+        self.loginAsPortalOwner()
+        typetool = self.portal.portal_types
+        typetool.constructContent('Document', self.portal, 'doc1')
+        self.typetool = typetool
+        self.portal_discussion = getToolByName(self.portal, 
+                                               'portal_discussion', 
+                                               None)
+        # we publish it
+        wft = getToolByName(self.portal, 'portal_workflow')
+        wft.doActionFor(self.portal.doc1, 'publish')
+        # and enable discussion on it
+        self.portal_discussion.overrideDiscussionFor(self.portal.doc1, True)
+        # Create a very long conversation with hundreds of comments
+        # so that we can start a ZServer and fiddle with firebug
+        conversation = IConversation(self.portal.doc1)
+        for i in range(300):
+            comment = createObject('plone.Comment')
+            comment.title = 'Comment %i' % i
+            comment.text = 'Comment %i text' % i
+            conversation.addComment(comment)
+        import Testing
+        host, port = Testing.ZopeTestCase.utils.startZServer()
+        obj_path = self.portal.doc1.virtual_url_path()
+        url = "http://%s:%i/%s" % (host, port, obj_path)
+        print
+        print url
+
+    def test_ajax_load(self):
+        import transaction
+        transaction.commit()
+        # Fire your favourite debugger (pdb obviously),
+        # open Firebug. and start coding!
+        # XXX this should become a real test
+        
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
+
